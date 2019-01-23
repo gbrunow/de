f3e08e066393897a565512c537b6f66d4c35f5c4
Guilherme Brunow <gbrunow@outlook.com>
2019-01-22 23:32:19 -0600

    shubert benchmark function

--- src/benchmark/simple.py
+++ src/benchmark/simple.py
@@ -15,3 +15,13 @@ def rosenbrock(Xs: np.matrix, a=1, b=100) -> np.matrix:
 def rastrigin(Xs: np.matrix, A=10) -> np.matrix:
     N = Xs.shape[0]
     return A * N + np.sum(Xs ** 2 - A * np.cos(2 * np.pi * Xs), axis=0)
+
+
+def shubert(x: np.array, y: np.array) -> np.array:
+    i = np.arange(1, 6).reshape(-1, 1)
+
+    return np.sum(
+        i * np.cos((i + 1) * x + i), axis=0
+    ) * np.sum(
+        i * np.cos((i + 1) * y + 1), axis=0
+    )
